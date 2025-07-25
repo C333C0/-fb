f92ecfb7d0c1e33f3356cc12583e869b21c2544a
C333C0 <swd43995@gmail.com>
2025-07-26 02:10:44 +0300

    config.py

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 import os
 
 
-API_ID: int = int(os.getenv("API_ID", 0))
+API_ID: int = int(os.getenv("API_ID", 25146373))
 
-API_HASH: str = os.getenv("API_HASH", "")
+API_HASH: str = os.getenv("API_HASH", "40aea6a22c2f6d05c71326663fc6f253")
 
-SESSION_STRING: str = os.getenv("SESSION_STRING", "")
+SESSION_STRING: str = os.getenv("SESSION_STRING", "AgG3g3kACGl_soZ317J-EiyFpNLbPLzVwZ2LhGycFT9IUAwmHVmZVjF8wN9XTi2DPI0Rf6qWc0IWGLSLsNIKCIkyAXi-M0t8ofb8Qkh7u8KhrqDt98112843xAKiFQXbmKLVQ6Y57Y1GQ9rxWfF3Z94zywGY7ep5w6IoZ07jw3yXFvoYeMkG5x3O2GUF7eWntQZcKRGmRS-e4VC7AWsQ6sS1R71qPRZf4I2_QWUzGLRo1c-Zm6mFBi9822E29OGJMq8w3V7Kqk4oiMS_ZnatIBWRRstU0h4vKWOi2saOxs_cWxYYVPmJ4-K-aU0ekfp5o0nSfgRGvA_2tfcr-Cz9EqktZkpSXgAAAAGV5F10AA")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 0))]
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 6809738612))]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", ksfhjs))
 
 PREFIX: str = str(".")
 
